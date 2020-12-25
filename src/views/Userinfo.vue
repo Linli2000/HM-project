d15fd289c07552469619b1aa393ac881bb33f5b5
unknown <you@example.com>
2020-12-25 17:58:00 +0800

    个人中心页面的布局完成

--- src/views/Userinfo.vue
+++ src/views/Userinfo.vue
@@ -1,39 +1,87 @@
 <template>
-  <div id="app">
-    <div class="header">头部</div>
-    <!-- 使用 Vant 组件库的 Cell 组件 -->
+  <!-- 个人中心
+   -->
+  <div>
+    <!-- 头部 -->
+    <div class="header">
+      <img class="avatar"
+           src="../assets/1.png"
+           alt="" />
+      <div class="info">
+        <p class="name">
+          <i class="iconfont iconxingbienv"></i>
+          <span>仙女姐姐</span>
+        </p>
+        <p class="date">2020-12-25</p>
+      </div>
+      <i class="iconfont iconjiantou1"></i>
+    </div>
+    <!-- 使用Vant组件库的Cell组件 -->
     <div class="divider"></div>
     <van-cell-group>
-      <van-cell title="我的关注" value="关注的用户" is-link />
-      <van-cell title="我的跟帖" value="跟帖/回复" is-link />
-      <van-cell title="我的收藏" value="文章/收藏" is-link />
-      <van-cell title="设置" is-link />
+      <van-cell title="我的关注"
+                value="关注的人"
+                is-link />
+      <van-cell title="我的跟帖"
+                value="跟帖/回复"
+                is-link />
+      <van-cell title="我的收藏"
+                value="文章/收藏"
+                is-link />
+      <van-cell title="设置"
+                is-link />
     </van-cell-group>
   </div>
 </template>
 
+
 <script>
-export default {
-  data () {
-    return {
-
-    }
-  },
-
-  methods: {
-
-  },
-
-  mounted () {
-
-  },
-}
+export default {};
 </script>
 
-<style lang = "less" scoped>
+
+
+
+<style lang="less" scoped>
 .header {
-  height: 120px;
-  background-color: tan;
+  height: 130px;
+  display: flex;
+  padding: 0 20px;
+  align-items: center;
+  .avatar {
+    width: 70px;
+    height: 70px;
+    border: 1px solid #ddd;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  .info {
+    flex: 1;
+    font-size: 14px;
+    p {
+      margin: 5px 0;
+    }
+    .name {
+      .iconxingbienv {
+        color: #8502c2;
+      }
+
+      span {
+        margin-left: 5px;
+        color: #333;
+      }
+    }
+
+    .date {
+      color: #999;
+    }
+  }
+
+  .iconjiantou1 {
+    color: #ccc;
+    font-size: 25px;
+  }
 }
 .divider {
   height: 10px;
